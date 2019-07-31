9edd1bff29d581068bcd64b4b2c440a36a2c6082
itsuncy <itsuncy@163.com>
2019-07-31 16:45:46 +0800

    123

---
+++ Chapter5/src/main/java/com/course/testng/multiThread/MultiThreadOnXml2.java
@@ -0,0 +1,50 @@
+package com.course.testng.multiThread;
+
+import org.testng.annotations.*;
+
+public class MultiThreadOnXml2 {
+
+
+    //Test注解，最基本的注解，用来把方法标记为测试的一部分
+    @Test
+    public void testCase1() {
+        System.out.printf("Thread Id : %s%n",Thread.currentThread().getId());
+        System.out.println("Test这是测试用例1");
+    }
+
+    @Test
+    public void testCase2() {
+        System.out.printf("Thread Id : %s%n",Thread.currentThread().getId());
+        System.out.println("Test这是测试用例2");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("beforeMethod这是在测试方法之前运行的");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("afterMethod这是在测试方法之后运行的");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("beforeClass这是在类运行之前运行的方法");
+    }
+
+    @AfterClass
+    public void AfterClass() {
+        System.out.println("AfterClass这是在类运行之前运行的方法");
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("beforeSuite测试套件");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("afterSuite测试套件");
+    }
+}
